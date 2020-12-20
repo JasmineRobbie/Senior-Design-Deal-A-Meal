66a794afbe0645842ea634299177dc2bb1e5d2de
Hussein226 <41562653+Hussein226@users.noreply.github.com>
2020-12-20 17:05:27 -0500

    Finished Message System
    Finished the message system,
    
    - Sending new Message works
    -replying to message works
    -deleting messages from inbox work

--- app/src/main/java/com/NeuralNet/AzureCloud/FullMessage2.java
+++ app/src/main/java/com/NeuralNet/AzureCloud/FullMessage2.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.teamcarl.prototype.R;
 
@@ -29,9 +31,11 @@ public class FullMessage2 extends Activity {
 
         String Body = intent.getExtras().getString("messageBody");
         String Subject = intent.getExtras().getString("messageSubject");
-        int senderId = intent.getExtras().getInt("messageSender");
-        int receiverId = intent.getExtras().getInt("messageReceiver");
+        final int senderId = intent.getExtras().getInt("messageSender");
+        final int receiverId = intent.getExtras().getInt("messageReceiver");
         int priority = intent.getExtras().getInt("messagePriority");
+
+
 
         user = (User)intent.getExtras().getSerializable("User");
 
@@ -49,7 +53,69 @@ public class FullMessage2 extends Activity {
             }
         });
 
+
+        replyBtn = findViewById(R.id.replyBtn);
+
+        replyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FullMessage2.this, ReplyMessage.class);
+
+                int newSender = receiverId;
+                int newReceiver = senderId;
+
+                System.out.println("Receiver ID: " + receiverId);
+                System.out.println("Sender ID: " + senderId);
+
+                System.out.println("New Receiver: " + newReceiver);
+                System.out.println("New Sender: " + newSender);
+
+                intent.putExtra("NewReceiver", newReceiver);
+                intent.putExtra("NewSender", newSender);
+
+
+                startActivity(intent);
+
+
+
+
+            }
+        });
+
+        deleteBtn = findViewById(R.id.deleteBtn);
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getIntent();
+
+                DataAccess db = new DataAccess();
+
+                int messageId = intent.getExtras().getInt("messageId");
+
+                try{
+
+                    String query = "DELETE FROM Message WHERE MessageId =" + messageId;
+
+                    String result = db.executeNonQuery(query);
+                    if(Integer.parseInt(result) > 0)
+                        Toast.makeText(getApplicationContext(), "Message Deleted Sucessfully", Toast.LENGTH_SHORT).show();
+                    else
+                        throw new Error("Error updating db. Message was not able to get deleted. ");
+                    finish();
+
+
+                }catch(Exception e){
+                    System.err.println(e);
+                }
+
+            }
+        });
+
+
+
     }
+
 
 
 
