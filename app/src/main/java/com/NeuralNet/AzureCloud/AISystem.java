c86e1526605ade1c3d9f6d4be7a4fca7cf5f253d
rhagavi <47401768+rhagavi@users.noreply.github.com>
2020-12-30 14:46:27 -0500

    Updated AI System and User Profile

--- app/src/main/java/com/NeuralNet/AzureCloud/AISystem.java
+++ app/src/main/java/com/NeuralNet/AzureCloud/AISystem.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.teamcarl.prototype.R;
 
@@ -26,7 +27,8 @@ public class AISystem extends Activity {
 
     int servingTotal;
     int totalCalories = 0;
-
+    int unitCalories,intProtein, intFat, intSatFat, intCarbs, intSugar;
+    String description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,13 +57,7 @@ public class AISystem extends Activity {
 
         consumeButton = findViewById(R.id.consumeButtonAI);
 
-
-        Random foodNum = new Random();
-        int result = foodNum.nextInt(6) + 1;
-
-
-
-
+        int result = 0;
         UpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,8 +66,13 @@ public class AISystem extends Activity {
                     servingTotal++;
                 }else{
                     servingTotal++;
+                    //let's try changing the values
+                    protein.setText(String.valueOf(intProtein * servingTotal));
+                    fat.setText(String.valueOf(intFat * servingTotal));
+                    satFat.setText(String.valueOf(intSatFat * servingTotal));
+                    carbs.setText(String.valueOf(intCarbs * servingTotal));
+                    sugar.setText(String.valueOf(intSugar * servingTotal));
                 }
-
 
                 String servingTotalString = String.valueOf(servingTotal);
                 servingAmount.setText(servingTotalString);
@@ -87,25 +88,60 @@ public class AISystem extends Activity {
                     servingTotal = 0;
                 }else{
                     servingTotal--;
+                    //let's try changing the values
+                    protein.setText(String.valueOf(intProtein * servingTotal));
+                    fat.setText(String.valueOf(intFat * servingTotal));
+                    satFat.setText(String.valueOf(intSatFat * servingTotal));
+                    carbs.setText(String.valueOf(intCarbs * servingTotal));
+                    sugar.setText(String.valueOf(intSugar * servingTotal));
                 }
 
                 String servingTotalString = String.valueOf(servingTotal);
                 servingAmount.setText(servingTotalString);
 
+
+            }
+        });
+        consumeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //Let's test out the values
+               System.out.println("Units Calories: " + unitCalories);
+               System.out.println("Protein: " + intProtein);
+               System.out.println("Fat: " + intFat);
+               System.out.println("SatFat: " + intSatFat);
+               System.out.println("Carbs: " + intCarbs);
+               System.out.println("Sugar: " + intSugar);
+               System.out.println("Serving total: " + servingTotal);
+               if(servingTotal > 0){
+                   //may need to call a class
+                   insertIntoEatenMeals(unitCalories,intProtein,intFat,intSatFat,intCarbs,intSugar,servingTotal, description);
+                   Toast.makeText(getApplicationContext(), "Meal Consumed", Toast.LENGTH_SHORT).show();
+                   finish(); // let's test this out
+               }
+               else if (servingTotal == 0){
+                   Toast.makeText(getApplicationContext(), "You have not consumed anything!!", Toast.LENGTH_SHORT).show();
+                   finish(); // let's test this out
+               }
+               else{
+                   System.out.println("There's an error with servingTotal!!!");
+               }
             }
         });
 
-        String getCaloriesQuery = "SELECT Units_Calories FROM EatenMeals WHERE UserId =" + User.UserID;
+        String getCaloriesQuery = "SELECT Units_Calories, servingAmount FROM EatenMeals WHERE UserId =" + User.UserID;
 
         final ResultSet results = db.getDataTable(getCaloriesQuery);
 
         ArrayList<String> calorieString = new ArrayList<>();
+        ArrayList<String> numberOfSServings = new ArrayList<>();
 
         try{
 
             while(results.next()){
 
                 calorieString.add(results.getString(1));
+                numberOfSServings.add(results.getString(2));
             }
 
         }catch(Exception e){
@@ -113,40 +149,61 @@ public class AISystem extends Activity {
         }
 
         for(int i = 0; i < calorieString.size(); i++){
-            totalCalories += Integer.parseInt(calorieString.get(i));
-
+            System.out.println("Total calories for " + i + " is: " + (Integer.parseInt(calorieString.get(i)) * Integer.parseInt(numberOfSServings.get(i))) );
+            totalCalories += (Integer.parseInt(calorieString.get(i)) * Integer.parseInt(numberOfSServings.get(i)));
         }
 
-
+        //Let's test out the totalCalories from database
+        System.out.println("Total calories: " + totalCalories);
 
         if(totalCalories <= 1000){
+            result = upToThousandCalories(totalCalories);
+            AITextBox.setText("Looks like you're under 1000 calories, eat this to bump up your calories!");
 
-            AITextBox.setText("Looks like youre under 1000 calories, eat this to bump up your calories!");
+        }else if(totalCalories > 1000 && totalCalories < 1600){
+            result = upToThousandCalories(totalCalories);
+            AITextBox.setText("Looks like you're under 2000 calories, eat this to bump up your calories!");
 
-        }else if(totalCalories > 1000){
-            AITextBox.setText("Looks like youre under 2000 calories, eat this to bump up your calories!");
-
-        }else if(totalCalories < 2000){
+        }else if(totalCalories >=1600 && totalCalories <2000){
+            result = meetingTheGoal(totalCalories);
             AITextBox.setText("Looks like you're close to hitting your daily calorie intake! Have this for today");
-
-        }else if(totalCalories > 2000){
-            AITextBox.setText("You're over your faily calorie intake, try having something small!");
+        }
+        else if(totalCalories == 2000){
+            result = reachedTheGoal(totalCalories);
+            AITextBox.setText("You have meet you daily expectation. Hungry?? Try this!!");
+        }
+        else if(totalCalories > 2000){
+            result = reachedTheGoal(totalCalories);
+            AITextBox.setText("You're over your daily calorie intake, try having something small!");
 
         }else{
             System.out.println("Else statement executed");
         }
 
-        String query = "SELECT Units_Calories, Units_Protein, Units_Fat, Units_SatFat, Units_Carbs, Units_Sugar FROM Today_Meals WHERE Today_Meals_ID =" + result;
+        String query = "SELECT Units_Calories, Units_Protein, Units_Fat, Units_SatFat, Units_Carbs, Units_Sugar, Description FROM Today_Meals WHERE Today_Meals_ID =" + result;
         final ResultSet rs = db.getDataTable(query);
 
         try{
 
             while(rs.next()){
+                unitCalories = rs.getInt(1);
+
                 protein.setText(rs.getString(2));
+                intProtein = rs.getInt(2);
+
                 fat.setText(rs.getString(3));
+                intFat = rs.getInt(3);
+
                 satFat.setText(rs.getString(4));
+                intSatFat = rs.getInt(4);
+
                 carbs.setText(rs.getString(5));
+                intCarbs = rs.getInt(5);
+
                 sugar.setText(rs.getString(6));
+                intSugar = rs.getInt(6);
+
+                description = rs.getString(7);
 
             }
 
@@ -154,10 +211,40 @@ public class AISystem extends Activity {
             System.out.println(e);
         }
 
+    }
+    //Let's call the class where you store consumed info into database
+    public void insertIntoEatenMeals(int unitCalories,int intProtein, int intFat, int intSatFat, int intCarbs, int intSugar,int servingTotal, String description){
+        //Accessing database
+                    System.out.println("UserID: " + User.UserID);
+                   DataAccess db = new DataAccess();
+                   String calories, protein, fat, satFat, carbs, sugar,servingTotalString, userID;
 
+                   calories = String.valueOf(unitCalories);
+                   protein = String.valueOf(intProtein);
+                   fat = String.valueOf(intFat);
+                   satFat = String.valueOf(intSatFat);
+                   carbs = String.valueOf(intCarbs);
+                   sugar = String.valueOf(intSugar);
+                   servingTotalString = String.valueOf(servingTotal);
+                   userID = User.UserID;
+                   System.out.println("Description: " + description);
 
+        String query = "INSERT INTO EatenMeals(UserId, Description, Units_Calories, " +
+                "Units_Protein, Units_Fat, Units_SatFat, Units_Carbs, Units_Sugar, servingAmount) \n"
+                + "VALUES ( '" + userID + "' , '" + description + "' , '" + calories +
+                "' , '" + protein + "' , '" + fat + "' , '" + satFat + "' , '" +
+                carbs + "' , '" + sugar + "' , '" + servingTotalString +"') \n";
 
-        switch(result){
+        db.executeNonQuery(query);
+        System.out.println("Entered eaten meal to database");
+
+    }
+    //Let's call the class where the totalCalories is up to 1599
+    public int upToThousandCalories(int totalCalories){
+        Random foodNum = new Random();
+        int resultValue = foodNum.nextInt(6) + 1;
+
+        switch(resultValue){
 
             case 1:
                 foodImage.setImageResource(R.drawable.orangejuice);
@@ -179,22 +266,58 @@ public class AISystem extends Activity {
                 break;
             default:
                 break;
-
-
-
         }
-
-
-
-
-
-
-
+        return resultValue;
     }
+    //Let's call the class where the totalCalories is from 1600 to 1999 calories
+    public int meetingTheGoal(int totalCalories){
+        Random foodNum = new Random();
+        int resultInt = foodNum.nextInt(5) + 1;
 
+        switch(resultInt){
 
+            case 1:
+                foodImage.setImageResource(R.drawable.orangejuice);
+                break;
+            case 2:
+                foodImage.setImageResource(R.drawable.englishmuffin);
+                break;
+            case 3:
+                foodImage.setImageResource(R.drawable.taco);
+                break;
+            case 4:
+                foodImage.setImageResource(R.drawable.snickers);
+                break;
+            case 5:
+                foodImage.setImageResource(R.drawable.sandwich);
+                break;
+            default:
+                break;
+        }
+        return resultInt;
+    }
+    //Let's call the class where the totalCalories is exactly 2000 calories and above
+    public int reachedTheGoal(int totalCalories){
+        Random foodNum = new Random();
+        int resultInt = foodNum.nextInt(4) + 1;
 
+        switch(resultInt){
 
-
-
+            case 1:
+                foodImage.setImageResource(R.drawable.orangejuice);
+                break;
+            case 2:
+                foodImage.setImageResource(R.drawable.englishmuffin);
+                break;
+            case 3:
+                foodImage.setImageResource(R.drawable.taco);
+                break;
+            case 4:
+                foodImage.setImageResource(R.drawable.snickers);
+                break;
+            default:
+                break;
+        }
+        return resultInt;
+    }
 }
